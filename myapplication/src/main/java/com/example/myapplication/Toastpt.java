32d2bfa8b692792418f91e003bef464fc928a071
Mubarik Nirban <mubariknirban786@gmail.com>
2021-09-22 10:55:26 +0530

    fff

--- myapplication/src/main/java/com/example/myapplication/Toastpt.java
+++ myapplication/src/main/java/com/example/myapplication/Toastpt.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class Toastpt {
-    
+
     public static void tst(Context c,String msz){
 
         Toast.makeText(c,msz,Toast.LENGTH_SHORT).show();
